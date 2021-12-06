e2d470128d4caa709d77fb45f2243aa72754c8ad
Zack Wolf <zackattack614@protonmail.com>
2021-12-05 20:55:30 -0500

    Add caching, connection to backend db

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM python:alpine
+FROM python:3.9
 
 WORKDIR /app
 ADD requirements.txt .
 
-RUN pip install -r requirements.txt
+RUN pip install -r requirements.txt && pip install --upgrade flask-api-cache
 ADD . .
 
 ENTRYPOINT ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-import glob
 import json
 from flask import Flask, Response, request
-app = Flask(__name__)
+from flask_api_cache import ApiCache
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
-data = dict()
-for dir in glob.glob('data/game_*'):
-  game_num = int(dir.split('_')[-1])
-  data[game_num] = dict()
-  for depth in [1, 5, 10, 15, 20]:
-    with open(f'{dir}/evals_depth_{depth}.json') as fin:
-      data[game_num][depth] = json.load(fin)
+cache_expiry_time = 100
+credentials = service_account.Credentials.from_service_account_file(
+    'lichess-analysis-abe717c29e85.json')
+bigquery_client = bigquery.Client(project='lichess-analysis', credentials=credentials)
+
+dataset_ref = bigquery_client.dataset('games')
+table_ref = dataset_ref.table('sharpness')
+table = bigquery_client.get_table(table_ref)
+
+app = Flask(__name__)
 
 def make_response(d):
   resp = Response(d)
@@ -17,69 +21,48 @@ def make_response(d):
   return resp
 
 @app.route('/games')
+@ApiCache(expired_time=cache_expiry_time)
 def games():
-  return make_response(json.dumps([key for key in data.keys()]))
-
-@app.route('/eval')
-def eval():
-  depth = request.args.get("depth")
-  game_num = request.args.get("game")
-  body = dict()
-
-  try:
-    depth = int(depth)
-    game_num = int(game_num)
-  except:
-    return make_response(body)
-
-  if depth is None or game_num is None:
-    return make_response(body)
-  if game_num not in data.keys():
-    return make_response(body)
-  if depth not in data.get(game_num).keys():
-    return make_response(body)
-
-  for i, (k, v) in enumerate(data[game_num][depth].items()):
-    body[k] = max(v.get('evals')) * (-1 if i % 2 == 1 else 1)
-
-  return make_response(json.dumps(body))
+  query = f'''
+    SELECT DISTINCT(game_number)
+    FROM `lichess-analysis.games.sharpness`
+  '''
+  r = list(bigquery_client.query(query).result())
+  return json.dumps([r[i]['game_number'] for i in range(len(r))])
 
 @app.route('/sharpness')
+@ApiCache(expired_time=cache_expiry_time)
 def sharpnesss():
   depth = request.args.get("depth")
   game_num = request.args.get("game")
   color = request.args.get("color")
 
   if depth is None or game_num is None:
-    return make_response({})
+    return make_response(json.dumps({}))
   try:
     depth = int(depth)
     game_num = int(game_num)
   except:
-    return make_response({})
+    return make_response(json.dumps({}))
 
-  if game_num not in data.keys():
-    return make_response({})
-  if depth not in data.get(game_num).keys():
-    return make_response({})
+  query = f'''
+    SELECT halfmove
+    FROM `lichess-analysis.games.sharpness`
+    WHERE game_number = {game_num} AND depth = {depth}
+  '''
+  r = list(bigquery_client.query(query).result())
+  if not any(r):
+    return make_response(json.dumps({}))
+
+  results = {str(i): v for i, v in enumerate(r[0][0])}
   if color is not None:
     if color.lower().strip() == 'white':
-      body = dict()
-      for i, (k, v) in enumerate(data[game_num][depth].items()):
-        if i % 2 == 1:
-          continue
-        body[k] = v
-      return make_response(json.dumps(body))
+      for i in range(1, len(results), 2):
+        del results[str(i)]
     elif color.lower().strip() == 'black':
-      body = dict()
-      for i, (k, v) in enumerate(data[game_num][depth].items()):
-        if i % 2 == 0:
-          continue
-        body[k] = v
-      return make_response(json.dumps(body))
-    else:
-      return make_response(json.dumps(data[game_num][depth]))
-  else:
-    return make_response(json.dumps(data[game_num][depth]))
+      for i in range(0, len(results), 2):
+        del results[str(i)]
+
+  return make_response(json.dumps(results))
 
 app.run(host='0.0.0.0')
